969a54f4e063ba6562a49a49dc9ee237abb07256
Corw <pzamyatin@gmail.com>
2016-07-05 23:15:20 +1200

    Added POST and PUT functions in API

--- app/post_routes.py
+++ app/post_routes.py
@@ -9,7 +9,7 @@ def get_post():
     """Searches the database for entries, then displays them."""
     posts = db.session.query(models.Post)
     posts_json = [post.json() for post in posts]
-    return jsonify({'posts' : posts_json})
+    return jsonify({'status' : 'ok'}, {'posts' : posts_json})
 
 @app.route('/post/', methods=['POST'])
 def add_post():
@@ -22,11 +22,21 @@ def add_post():
     db.session.add(post)
     db.session.commit()
 
-    return jsonify({'status' : 'ok'}), 201
+    return jsonify({'status' : 'ok'}, {'post' : post.json()}), 201
 
 @app.route('/post/<int:post_id>', methods=['PUT'])
-def edit_post():
-    pass
+def edit_post(post_id):
+    """Edit post in the database."""
+    post = models.Post.query.get(post_id)
+    content = request.json.get('text', '')
+
+    if post is None or len(content) > 140 or len(content) == 0:
+        return jsonify({'status' : 'failed'}), 400
+
+    post.text = content
+    db.session.commit()
+
+    return jsonify({'status' : 'ok'}, {'post' : post.json()}), 201
 
 @app.route('/post/<int:post_id>', methods=['DELETE'])
 def remove_post():
