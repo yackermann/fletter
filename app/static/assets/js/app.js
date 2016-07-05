da6ed82741c96b53b85bb0483e3545395844582b
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-07-05 17:03:41 +1200

    Implemented add and get api methods. Added basic form processor.

--- app/static/assets/js/app.js
+++ app/static/assets/js/app.js
@@ -1,1 +1,44 @@
-$(document).foundation()
+(function(){
+    $(document).foundation();
+
+    _post_api_url         = '/post';
+
+    $new_post_form        = $('#new_post');
+    _new_post_field       = '.post__new_post';
+    _new_post__size_limit = 140;
+
+    var $make_request = function (URL, METHOD, DATA, CALLBACK) {
+        $.ajax({
+            url: URL,
+            type: METHOD,
+            data: DATA,
+            success: CALLBACK,
+            fail: function(err){
+                CALLBACK({'status': 'failed', 'error': err})
+            }
+        });
+    }
+
+    var $api = {
+        'add' : function (DATA) {
+            $make_request(_post_api_url, 'POST', DATA, (data) => console.log(data))
+        },
+
+        'get' : function (ID) {
+            _URL = _post_api_url;
+            if(ID && typeof ID == 'number')
+                _URL += '/' + ID;
+
+            $make_request(_URL, 'GET', {}, (data) => console.log(data))
+        }
+    }
+
+    $new_post_form.submit(function (event) {
+        event.preventDefault();
+        var content = $(_new_post_field, this).val();
+
+        if(content && content.length <= _new_post__size_limit){
+            $api.add({'text': content})
+        }
+    })
+})()
