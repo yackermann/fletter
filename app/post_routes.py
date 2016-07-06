150dc08412f323af8cb90322fbc2a341f54ddb4b
Corw <pzamyatin@gmail.com>
2016-07-06 14:52:13 +1200

    Implemented post API according to docs #3 #4 #5 #6#13

--- app/post_routes.py
+++ app/post_routes.py
@@ -10,29 +10,51 @@ def get_post(post_id=None):
     if post_id is None:
         posts = db.session.query(models.Post)
         posts_json = [post.json() for post in posts]
-        return jsonify({'status' : 'ok'}, {'posts' : posts_json})
+        return jsonify({
+            'status' : 'ok',
+            'posts' : posts_json
+        })
+
     else:
         post = models.Post.query.get(post_id)
 
         if post is None:
-            return jsonify({'status' : 'failed'}), 400
+            return jsonify({
+                'status' : 'failed',
+                'error'  : 'Post not found!'
+            }), 404
 
         post_json = post.json()
-        return jsonify({'status' : 'ok'}, {'post' : post_json})
+
+        return jsonify({
+            'status' : 'ok',
+            'post' : post_json
+        })
 
 
 @app.route('/post/', methods=['POST'])
 def add_post():
     """Adds new post to the database."""
     content = request.json.get('text', '')
-    if len(content) > 140 or len(content) == 0:
-        return jsonify({'status' : 'failed'}), 400 
+    if len(content) > 140:
+        return jsonify({
+            'status' : 'failed',
+            'error'  : 'Text is longer than 140 chars!'
+        }), 413
+    if len(content) == 0:
+        return jsonify({
+            'status' : 'failed',
+            'error'  : 'Text is missing!'
+        }), 400
 
     post = models.Post(content)
     db.session.add(post)
     db.session.commit()
 
-    return jsonify({'status' : 'ok'}, {'post' : post.json()}), 201
+    return jsonify({
+        'status' : 'ok',
+        'post' : post.json()
+    }), 201
 
 @app.route('/post/<int:post_id>', methods=['PUT'])
 def edit_post(post_id):
@@ -40,13 +62,31 @@ def edit_post(post_id):
     post = models.Post.query.get(post_id)
     content = request.json.get('text', '')
 
-    if post is None or len(content) > 140 or len(content) == 0:
-        return jsonify({'status' : 'failed'}), 400
+    if post is None:
+        return jsonify({
+            'status' : 'failed',
+            'error'  : 'Post not found!'
+        }), 404
+
+    if len(content) > 140:
+        return jsonify({
+            'status' : 'failed',
+            'error'  : 'Text is longer than 140 chars!'
+        }), 413
+
+    if len(content) == 0:
+        return jsonify({
+            'status' : 'failed',
+            'error'  : 'Text is missing!'
+        }), 400
 
     post.text = content
     db.session.commit()
 
-    return jsonify({'status' : 'ok'}, {'post' : post.json()}), 201
+    return jsonify({
+        'status' : 'ok',
+        'post' : post.json()
+    }), 200
 
 @app.route('/post/<int:post_id>', methods=['DELETE'])
 def remove_post(post_id):
@@ -54,7 +94,10 @@ def remove_post(post_id):
     post = models.Post.query.get(post_id)
 
     if post is None:
-        return jsonify({'status' : 'failed'}), 400
+        return jsonify({
+            'status' : 'failed',
+            'error'  : 'Post not found!'
+        }), 404
 
     db.session.delete(post)
     db.session.commit()
