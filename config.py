d9b301c15301b4d558a04229bf7ba172718a2bda
Corw <pzamyatin@gmail.com>
2016-07-07 14:27:06 +1200

    Added basic log system

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os, logging
+from logging import Formatter
 
 WTF_CSRF_ENABLED = True
 
@@ -18,3 +19,12 @@ DATABASE_PATH = os.path.join(basedir, DATABASE)
 # the database uri
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
 SQLALCHEMY_TRACK_MODIFICATIONS = 'True'
+
+# Configure logging
+LOG_FILE = 'tmp/logfile.log'
+LOG_LEVEL = logging.DEBUG
+
+logging.basicConfig(filename = LOG_FILE,
+                    level    = LOG_LEVEL)
+
+logging.getLogger().addHandler(logging.StreamHandler())
